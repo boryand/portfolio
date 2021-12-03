4b77b7d92f9611d03163ee6de8651400d21e93f7
boryand <boryand>
2021-12-03 16:23:17 +0100

    First changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Switch, Route, Link, NavLink } from "react-router-dom";
+import Navbar from './components/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="navbar">
+        <ul className="navbar1">
+          <li className="navbar2"> <button className="neonShadow"> <span>HOME </span> </button></li>
+          {/* <li className="navbar2"> <button className="neonShadow"> <span> HOME </span> </button> </li> */}
+          <li className="navbar2"> <button className="neonShadow"> <span> HOME </span> </button> </li>
+          </ul>
+      
+      </div>
+      {/* <Switch>
+      <Route path="/home">
+              <Navbar />
+            </Route>
+      </Switch> */}
+     
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+import react from 'react'
+
+const Navbar = ()=> {
+
+return (
+    <div>
+        <p> HEL </p>
+        </div>
+)
+
+
+}
+
+export default Navbar ;
