452780899cdc74cc20c8e06ab45e9d592cf59df7
boryand <boryand>
2022-02-11 10:16:21 +0100

    Adding one more project

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,60 +1,55 @@
-import comming from "../pictures/comming.gif"
-import pika from "../pictures/pika.png"
+import comming from "../pictures/comming.gif";
+import pika from "../pictures/pika.png";
+import logo from "../pictures/logo.png"
+import { LOADING } from "@blueprintjs/core/lib/esm/common/classes";
 
+const Projects = () => {
+  const url = "https://pokefight123.herokuapp.com/";
+  const url2 = "https://max-lounge.herokuapp.com/"
+  return (
+    <div className="container5">
+      {/* <h1> Click on a card to see the project</h1> */}
 
-const Projects = ()=> {
-    const url = "https://pokefight123.herokuapp.com/"
-    return (
-        <div className="container5">
-         
-
-   <h1> Click on a card to see the project</h1>
-
-
-
-
-
-
-
-
-
-
-         <div class="card">
-    <div class="image">
-      <img src={pika}/>
-    </div>
-    <div class="details">
-      <div class="center">
-        <h1>Pokefight<br /></h1>
-        <p>This project is not my first , but one of <br/>
-        my favorites.  There was an API <br />
-        provided for me and my colleague and we mainly did the front-end.
-        The game mechanism is really effortless - the winning pokemon <br/>
-        is the one with the biggest HP.
-      </p>
-        <a className="project_link" href={url}><span>Check it out</span></a>
-       
-      </div>
-    </div>
-  </div>
-
-
-
-
-
-
-
-
-
-
+      <div class="card">
+        <div class="image">
+          <img src={pika} />
         </div>
+        <div class="details">
+          <div class="center">
+            <h1>
+              Pokefight
+              <br />
+            </h1>
+            <p>What I used: <br />🌟 HTML/CSS <br /> 🌟 Javascript <br /> 🌟React</p>
+            <a className="project_link" href={url}>
+              <span>Check it out</span>
+            </a>
+          </div>
+        </div>
+      </div>
 
 
+      <div class="card">
+        <div class="image">
+          <img src={logo} />
+        </div>
+        <div class="details">
+          <div class="center">
+            <h1>
+              Max Lounge
+              <br />
+            </h1>
+            <p>What I used: <br />🌟 HTML/Sass <br /> 🌟 Javascript <br /> 🌟React</p>
+            <a className="project_link" href={url2}>
+              <span>Check it out</span>
+            </a>
+          </div>
+        </div>
+      </div>
 
 
-
-
-    )
-}
+    </div>
+  );
+};
 
 export default Projects;
